4ee8637e2c477c6f3071da91d10851e2b439205f
ddvp <ddvp0188@gmail.com>
2022-03-06 02:35:07 -0600

    Delete funcion_grados.js

--- funcion_grados.js
+++
@@ -1,9 +0,0 @@
-/*Instrucciones: Revisar que las pruebas pasen, corregir las pruebas y 
-hacer ajustes en esta función para que haga las validaciones que estan en las pruebas*/
-function convertir(celsius) {
-    let fahrenheit = celsius* (9 / 5) + 32;
-   // fahrenheit = Math.floor(fahrenheit);
-    return fahrenheit;
-}
-
-module.exports = convertir;
